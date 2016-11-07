d3f5061697daf8639a67a75723f95eea6b1125d0
Salomon Turgman <sturgman@gmail.com>
2016-11-07 12:30:05 -0500

    I can generate and render a list of bonds. However, I cannot render them because I have to somehow make reference to the atom positions. I am having trouble figuring how to do that.

--- main.elm
+++ main.elm
@@ -9,6 +9,7 @@ import String
 import List.Extra
 import List
 
+
 main =
     App.beginnerProgram { model = model, view = view, update = update }
 
@@ -38,6 +39,7 @@ model =
     
 type alias Atom =
     { atype : Int
+    , anum : Int
     , x : Int
     , y : Int
     } 
@@ -49,7 +51,11 @@ type alias Bond =
     }
 
 
-type Msg = Changex String| Changey String | Generate
+type Msg
+    = Changex String
+    | Changey String
+    | Generate
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -57,43 +63,132 @@ update msg model =
         -- I am converting strings to ints here...
         -- It took me a long time to realize how to do this.
         -- In particular: nested case statements! And general use of Result.
-        Changex str -> case String.toInt str of
-                           Ok numx ->
-                               if (numx < 50 && model.numyatoms < 50) then {model | numxatoms = numx, valid = True} else {model | numxatoms = numx, valid = False}
-                           Err err -> {model | valid = False}
-        Changey str -> case String.toInt str of
-                           Ok numy -> if (numy < 50 && model.numxatoms < 50) then {model | numyatoms = numy , valid = True} else {model | numyatoms = numy, valid = False}
-                           Err err -> {model | valid = False}
-        Generate -> if model.valid == True then makeSheet model else model
+        Changex str ->
+            case String.toInt str of
+                Ok numx ->
+                    if (numx < 50 && model.numyatoms < 50) then
+                        {model | numxatoms = numx, valid = True}
 
+                    else
+                        {model | numxatoms = numx, valid = False}
+                            
+                Err err ->
+                    {model | valid = False}
+
+        Changey str ->
+            case String.toInt str of
+                Ok numy ->
+                    if (numy < 50 && model.numxatoms < 50) then
+                        {model | numyatoms = numy , valid = True}
+                            
+                    else
+                        {model | numyatoms = numy, valid = False}
+
+                Err err ->
+                    {model | valid = False}
+
+        Generate ->
+            if model.valid == True then
+                makeSheet model
+
+            else model
+
+                
 makeSheet : Model -> Model
 makeSheet model =
     model
         |> makeAtoms
--- once bond are implemented |> makeBonds
+        |> makeBonds
+
 
 makeAtoms : Model -> Model
 makeAtoms model =
     let
-        x = [1..model.numxatoms]
-        y = [1..model.numyatoms]
-        makeAtom : Int -> (Int,Int) -> Atom
-        makeAtom t coord =
+        x =
+            [1..model.numxatoms]
+
+        y =
+            [1..model.numyatoms]
+
+        makeAtom : Int -> Int -> (Int,Int) -> Atom
+        makeAtom t idx coord =
             let
-                (xn,yn)=coord
+                (yn,xn)=coord
             in
-                {atype=t,x=xn,y=yn}
+                {atype=t,anum=idx,x=xn,y=yn}
 
         -- This is for future functionality
         makeAtomT = makeAtom 1
-        listcoords=List.Extra.lift2 (,) x y
+        listcoords=List.Extra.lift2 (,) y x
     in
-        {model | listatoms= List.map makeAtomT listcoords}
+        {model | listatoms= List.indexedMap makeAtomT listcoords}
+
+
+makeBonds : Model -> Model
+makeBonds model =
+    let
+        makeBond : Int -> Atom -> List Bond
+        makeBond idx atom =
+            if (atomRegular atom model) then
+                List.append (makebondright idx) (makebonddown idx model)
+                    
+            else if (atomFinalColumn atom model) then
+                makebonddown idx model
+                
+            else if (atomFinalRow atom model) then
+                makebondright idx
+
+            else 
+                []
+
+    in
+        {model | listbonds = List.concat (List.indexedMap makeBond model.listatoms)}
+
+             
+makebondright : Int -> List Bond
+makebondright id =
+    [
+     { btype = 1
+     , at1 = id
+     , at2 = id + 1
+     }
+    ]
+
+
+makebonddown : Int -> Model -> List Bond
+makebonddown id model =
+    [
+     { btype = 1
+     , at1 = id
+     , at2 = id + model.numxatoms
+     }
+    ]
+
+atomRegular : Atom -> Model -> Bool
+atomRegular atom model =
+    if atom.x < model.numxatoms && atom.y < model.numyatoms then True else False
+
+atomFinalColumn : Atom -> Model -> Bool
+atomFinalColumn atom model =
+    if atom.x == model.numxatoms && atom.y < model.numyatoms then True else False
+
+atomFinalRow : Atom -> Model -> Bool
+atomFinalRow atom model =
+    if atom.x < model.numxatoms && atom.y == model.numyatoms then True else False
+
 
 -- What is the type annotation for this? I have no idea. This particular snippet was borrowed from stack overflow. I THINK I understand what is happening
-renderList lst =
+renderListAtoms lst =
     lst
-    |> List.map (\l -> li [] [ text ((toString l.x) ++ "  " ++ (toString l.y)) ])
+    |> List.map (\l -> li [] [ text
+                               ((toString l.x) ++ "  " ++ (toString l.y)) ])
+    |> ul []
+
+
+renderListBonds lst =
+    lst
+    |> List.map (\l -> li [] [ text
+                               ((toString (l.at1+1)) ++ "  " ++ (toString (l.at2+1))) ])
     |> ul []
 
 
@@ -104,20 +199,31 @@ renderSvg lst =
             Svg.circle [ Svga.cx ((toString atm.x) ++ "%")
                        , Svga.cy ((toString atm.y) ++ "%")
                        , Svga.r "0.25%"] []
+    {-                
+        renderBond bnd lst=
+            Svg.line [ Svga.x1 -}
     in
         Svg.svg [ Svga.width "100%"
                 , Svga.height "100%"
                 , Svga.viewBox "0 0 1000 1000"
                 ] (List.map renderCircle lst)
 
+            
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [text "This is work in progress! A tool to write coordiantes and bonds for a sheet. Input numbers less than 50."]
+        [ div [] [text "This is work in progress! A tool to write coordiantes
+                        and bonds for a sheet. Input numbers less than 50."]
         , input [ placeholder "Number of atoms in x direction", onInput Changex ] []
         , input [ placeholder "Number of atoms in y direction", onInput Changey ] []
         ,button [ onClick Generate ] [ Html.text "Generate Sheet" ]
-        , div [] [text ("Model is " ++ (if model.valid then "ready" else "too big, disabled for now."))]
+        , div [] [text
+                  ("Model is " ++ (if model.valid then
+                                       "ready"
+                                   else
+                                       "too big, disabled for now."))]
         , div [] [renderSvg model.listatoms]
--- only for debugging       , div [] [renderList model.listatoms]
+        , div [] [renderListBonds model.listbonds]
+        , div [] [renderListAtoms model.listatoms]
+
         ]
